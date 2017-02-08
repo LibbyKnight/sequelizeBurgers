66ddf09cf5ea05a33d2a3980ee157f38435bc506
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-02-08 13:54:44 -0500

    made some changes. didnt work

--- skeleton/controllers/burgers_controller.js
+++ skeleton/controllers/burgers_controller.js
@@ -1,14 +1,14 @@
 var express = require("express");
 
 var router = express.Router();
-var Burger = require("../models/burger");
+var Burger = require("../models/burger.js");
 
 // get route -> index
 
 
 router.get("/api/burgers", function(req, res) {
   // express callback response by calling burger.selectAllBurger
-  Burger.findAll({}).then(function(data) {
+  Burger.burgers.findAll({})(function(data) {
     // Wrapping the array of returned burgers in a object so it can be referenced inside our handlebars
     var hbsObject = { burgers: data };
     res.render("index", hbsObject);
@@ -18,7 +18,7 @@ router.get("/api/burgers", function(req, res) {
 // post route -> back to index
 router.post("/api/burgers", function(req, res) {
   // takes the request object using it as input for buger.addBurger
-  Burger.create({
+  Burger.burgers.create({
     burgerName: req.body.burger_name
 }).then(function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
@@ -30,7 +30,7 @@ router.post("/api/burgers", function(req, res) {
 
 // put route -> back to index
 router.put("/api/burgers", function(req, res) {
-  Burger.update({
+  Burger.burgers.update({
     burgerId: req.body.burger_id
   }).then(function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
